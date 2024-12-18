32342b74a6a8deae291841d2525ae69a56969dc5
divya-ct <guddan.282005@gmail.com>
2024-12-19 00:30:49 +0530

    created

--- sun.cpp
+++ sun.cpp
@@ -6,9 +6,10 @@ int main()
 	
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm,NULL);
-	int midx,midy,r=10;
-	midx=getmaxx()/2;
-	while(r<=50)
+	int midx,midy,r=10;     int midx,midy,r=50;
+	midx=getmaxx()/2;        
+	                        midy=310;
+	while(r<=50)            while(r>=10)
 	{
 		cleardevice();
 		setcolor(WHITE);
@@ -17,13 +18,14 @@ int main()
 		line(320,310,480,150);
 		line(480,150,640,310);
 		line(0,310,640,310);
-		arc(midx,310,225,133,r);
-		floodfill(midx,300,15);
-		if(r>20)
+		                              setcolor(14);
+		arc(midx,310,225,133,r);      arc(midx,midy,0,180,r);
+		floodfill(midx,300,15);       floodfill(midx,midy+5,15);
+		if(r>20)                      if(r<40)
 		{
-			setcolor(7);
+			setcolor(7);          setcolor(12);
 			floodfill(2,2,15);
-			setcolor(6);
+			setcolor(6);          setcolor(14);
 			floodfill(150,250,15);
 			floodfill(550,250,15);
 			setcolor(2);
@@ -31,7 +33,7 @@ int main()
 		}
 
 		delay(50);
-		r+=2;
+		r+=2;                           r-=2;
 	}
 	getch();
 	closegraph();
